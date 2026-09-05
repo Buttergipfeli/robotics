0b571a15842bd32de7d98352b752bb27c5044b2c
Andre Kocher <andre01kocher@gmail.com>
2026-09-05 21:44:23 +0200

    Refactor rename basket to toilet roll

--- lerobot/sim/sim_env.py
+++ lerobot/sim/sim_env.py
@@ -8,10 +8,10 @@ class So101PickBallEnv:
 
     MAT_X = (0.1366, 0.3866)
     MAT_Y = (-0.15, 0.15)
-    BALL_RADIUS = 0.025
-    BASKET_RADIUS = 0.05
-    MIN_BALL_BASKET_DIST = 0.10
-    BALL_Z = 0.016
+    BALL_RADIUS = 0.0334
+    BALL_Z = 0.0244
+    ROLL_RADIUS = 0.0605
+    MIN_BALL_ROLL_DIST = 0.115
 
     def __init__(self, model, seed=None):
         self.model = model
@@ -25,12 +25,12 @@ class So101PickBallEnv:
     def reset(self):
         mujoco.mj_resetDataKeyframe(self.model, self.data, 0)
         self.data.time = 0.0
-        self.data.t = 0.0
+        self.t = 0
 
-        margin = self.BASKET_RADIUS + 0.01
-        basket_xy = self.rng.uniform(
+        margin = self.ROLL_RADIUS + 0.01
+        roll_xy = self.rng.uniform(
             [self.MAT_X[0] + margin, self.MAT_Y[0] + margin],
             [self.MAT_X[1] - margin, self.MAT_Y[1] - margin],
         )
-        self.model.body("basket").pos[:2] = basket_xy
+        self.model.body("toilet_roll").pos[:2] = roll_xy
         #return self.get_observation()
