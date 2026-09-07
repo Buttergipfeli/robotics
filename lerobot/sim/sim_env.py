531f9256f7324daefc5259f13db19cc9789e250e
Andre Kocher <andre01kocher@gmail.com>
2026-09-07 11:17:52 +0200

    Randomize domain per episode for sim-to-real robustness

--- lerobot/sim/sim_env.py
+++ lerobot/sim/sim_env.py
@@ -8,8 +8,7 @@ class So101PickBallEnv:
     CAM_WIDTH = 640
     CAM_HEIGHT = 480
 
-    MAT_X = (0.1366, 0.3866)
-    MAT_Y = (-0.15, 0.15)
+    MAT_BODY = "cork_mat"
     MAT_EDGE_MARGIN = 0.01
 
     BALL_RADIUS = 0.0334
@@ -42,10 +41,22 @@ class So101PickBallEnv:
     TCP_SITE = "gripperframe"
     BALL_HOLD_POS = (0.020, 0.0, -0.090)
 
-    def __init__(self, model, seed=None):
+    MAT_SHIFT = 0.01
+    BALL_FRICTION_RANGE = (0.8, 2.5)
+    BALL_MASS_SCALE_RANGE = (0.9, 1.1)
+    CAM_POS_JITTER = 0.008
+    CAM_TILT_JITTER = 0.05
+    CAM_FOVY_RANGE = (64.0, 76.0)
+    LIGHT_POS_JITTER = 0.3
+    LIGHT_DIFFUSE_SCALE_RANGE = (0.6, 1.2)
+    MAT_SHADE_SCALE_RANGE = (0.8, 1.15)
+    PAPER_SHADE_SCALE_RANGE = (0.85, 1.02)
+
+    def __init__(self, model, seed=None, randomize=True):
         self.model = model
         self.data = mujoco.MjData(self.model)
         self.rng = np.random.default_rng(seed)
+        self.randomize = randomize
         self.n_substeps = int(1 / (self.CONTROL_HZ * self.model.opt.timestep))
         self.max_steps = self.CONTROL_HZ * self.EPISODE_SECONDS
         self.renderer = mujoco.Renderer(self.model, height=self.CAM_HEIGHT, width=self.CAM_WIDTH)
@@ -58,6 +69,28 @@ class So101PickBallEnv:
         self.grip_body_id = self.model.body("gripper").id
         self.ball_body_id = self.model.body("stress_ball").id
         self.grasp_eq_id = self.model.equality("grasp_assist").id
+
+        self.mat_body_id = self.model.body(self.MAT_BODY).id
+        self.mat_geom_id = next(
+            g for g in range(self.model.ngeom) if self.model.geom_bodyid[g] == self.mat_body_id
+        )
+        self.mat_half = self.model.geom_size[self.mat_geom_id][:2].copy()
+        self.ball_geom_id = self.model.geom("stress_ball").id
+        self.cam_id = self.model.camera("wrist").id
+        self.paper_geom_ids = [
+            g for g in range(self.model.ngeom)
+            if (mujoco.mj_id2name(self.model, mujoco.mjtObj.mjOBJ_GEOM, g) or "").startswith("roll_paper")
+        ]
+
+        self.base_mat_pos = self.model.body_pos[self.mat_body_id].copy()
+        self.base_mat_rgba = self.model.geom_rgba[self.mat_geom_id].copy()
+        self.base_paper_rgba = self.model.geom_rgba[self.paper_geom_ids[0]].copy()
+        self.base_ball_mass = float(self.model.body_mass[self.ball_body_id])
+        self.base_ball_inertia = self.model.body_inertia[self.ball_body_id].copy()
+        self.base_cam_pos = self.model.cam_pos[self.cam_id].copy()
+        self.base_cam_quat = self.model.cam_quat[self.cam_id].copy()
+        self.base_light_pos = self.model.light_pos[0].copy() if self.model.nlight else None
+        self.base_light_diffuse = self.model.light_diffuse[0].copy() if self.model.nlight else None
 
     def launch(self):
         mujoco.viewer.launch(self.model, self.data)
@@ -116,8 +149,57 @@ class So101PickBallEnv:
 
     def is_ball_lost(self):
         x, y, z = self.data.qpos[self.BALL_POS]
-        on_mat = self.MAT_X[0] < x < self.MAT_X[1] and self.MAT_Y[0] < y < self.MAT_Y[1]
+        mat_x, mat_y = self._mat_bounds()
+        on_mat = mat_x[0] < x < mat_x[1] and mat_y[0] < y < mat_y[1]
         return z < self.BALL_LOST_MAX_Z and not on_mat
+
+    def _mat_bounds(self):
+        pos = self.model.body_pos[self.mat_body_id]
+        return (
+            (pos[0] - self.mat_half[0], pos[0] + self.mat_half[0]),
+            (pos[1] - self.mat_half[1], pos[1] + self.mat_half[1]),
+        )
+
+    def _randomize_domain(self):
+        rng = self.rng
+        model = self.model
+
+        model.body_pos[self.mat_body_id][:2] = self.base_mat_pos[:2] + rng.uniform(
+            -self.MAT_SHIFT, self.MAT_SHIFT, 2
+        )
+        model.geom_friction[self.ball_geom_id][0] = rng.uniform(*self.BALL_FRICTION_RANGE)
+        mass_scale = rng.uniform(*self.BALL_MASS_SCALE_RANGE)
+        model.body_mass[self.ball_body_id] = self.base_ball_mass * mass_scale
+        model.body_inertia[self.ball_body_id] = self.base_ball_inertia * mass_scale
+
+        model.cam_pos[self.cam_id] = self.base_cam_pos + rng.uniform(
+            -self.CAM_POS_JITTER, self.CAM_POS_JITTER, 3
+        )
+        quat = self.base_cam_quat.copy()
+        for axis in ([1.0, 0.0, 0.0], [0.0, 1.0, 0.0]):
+            tilt = np.zeros(4)
+            mujoco.mju_axisAngle2Quat(
+                tilt, np.array(axis), rng.uniform(-self.CAM_TILT_JITTER, self.CAM_TILT_JITTER)
+            )
+            rotated = np.zeros(4)
+            mujoco.mju_mulQuat(rotated, quat, tilt)
+            quat = rotated
+        model.cam_quat[self.cam_id] = quat
+        model.cam_fovy[self.cam_id] = rng.uniform(*self.CAM_FOVY_RANGE)
+
+        mat_shade = rng.uniform(*self.MAT_SHADE_SCALE_RANGE)
+        model.geom_rgba[self.mat_geom_id][:3] = np.clip(self.base_mat_rgba[:3] * mat_shade, 0, 1)
+        paper_shade = rng.uniform(*self.PAPER_SHADE_SCALE_RANGE)
+        for g in self.paper_geom_ids:
+            model.geom_rgba[g][:3] = np.clip(self.base_paper_rgba[:3] * paper_shade, 0, 1)
+
+        if self.model.nlight:
+            model.light_pos[0][:2] = self.base_light_pos[:2] + rng.uniform(
+                -self.LIGHT_POS_JITTER, self.LIGHT_POS_JITTER, 2
+            )
+            model.light_diffuse[0] = self.base_light_diffuse * rng.uniform(
+                *self.LIGHT_DIFFUSE_SCALE_RANGE
+            )
 
     def get_observation(self):
         self.renderer.update_scene(self.data, camera="wrist")
@@ -131,11 +213,15 @@ class So101PickBallEnv:
         self.t = 0
         self.success_streak = 0
 
+        if self.randomize:
+            self._randomize_domain()
+        mat_x, mat_y = self._mat_bounds()
+
         margin = self.ROLL_RADIUS + self.MAT_EDGE_MARGIN
         while True:
             roll_xy = self.rng.uniform(
-                [self.MAT_X[0] + margin, self.MAT_Y[0] + margin],
-                [self.MAT_X[1] - margin, self.MAT_Y[1] - margin],
+                [mat_x[0] + margin, mat_y[0] + margin],
+                [mat_x[1] - margin, mat_y[1] - margin],
             )
             if np.linalg.norm(roll_xy - self.ARM_REST_XY) >= self.ROLL_RADIUS + self.MIN_ROLL_ARM_CLEARANCE:
                 break
@@ -143,8 +229,8 @@ class So101PickBallEnv:
 
         while True:
             ball_xy = self.rng.uniform(
-                [self.MAT_X[0] + self.BALL_RADIUS, self.MAT_Y[0] + self.BALL_RADIUS],
-                [self.MAT_X[1] - self.BALL_RADIUS, self.MAT_Y[1] - self.BALL_RADIUS],
+                [mat_x[0] + self.BALL_RADIUS, mat_y[0] + self.BALL_RADIUS],
+                [mat_x[1] - self.BALL_RADIUS, mat_y[1] - self.BALL_RADIUS],
             )
             if (
                 np.linalg.norm(ball_xy - roll_xy) >= self.MIN_BALL_ROLL_DIST
